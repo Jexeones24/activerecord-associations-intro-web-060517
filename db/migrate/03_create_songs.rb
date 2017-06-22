2808fe37bfd0b187c5682ca514968e85bd43bde0
Jexeones24 <jexeones@gmail.com>
2017-06-22 11:59:46 -0400

    Done

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
-class CreateSongs < ActiveRecord::Migration
+class CreateSongs < ActiveRecord::Migration[5.0] #join table
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :artist_id #foreign_key
+      t.integer :genre_id #foreign_key
+    end
+  end
 end
